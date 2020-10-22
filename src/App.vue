04169b4a8ca261498561849be10539b7336a881d
wanwan <1171317749@qq.com>
2020-10-22 13:59:05 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <a-button type="primary">Button</a-button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
